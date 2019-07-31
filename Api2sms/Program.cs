a62bab687cef4682cb26c239b5796f7fcccabac7
Andrey Shumaev <shumaev@bk.ru>
2019-07-31 13:41:34 +0300

    Финальная версия

--- Api2sms/Program.cs
+++ Api2sms/Program.cs
@@ -12,30 +12,30 @@ namespace Api2sms
             try
             {
 				string urlResponse = "https://sms.ru";
+				//Если нет параметров выводим инфу и на выход
 				if (args.Length == 0)
                 {
                     Console.WriteLine("Использование:   api2sms [API number message]");
-                    Console.WriteLine("                         [API --balanse]");
+                    Console.WriteLine("                         [API -balance]");
                     Console.WriteLine("Параметры:");
                     Console.WriteLine("     API             ключ API на sms.ru");
                     Console.WriteLine("     number          номер телефона в формате 7XXXXXXXXXX");
                     Console.WriteLine("     message         сообщение, пробелы заменить на символ +");
-                    Console.WriteLine("     --balanse       выводит баланс на sms.ru");
+                    Console.WriteLine("     -balance        выводит баланс на sms.ru");
                     Console.WriteLine("");
                     System.Environment.Exit(1);
                 }
 				string API = args[0];
-				if (args[0] == "shumaev") API = "0b3aa42c-d701-7064-e12d-3fd20a5d154b";
-				if (args[1] == "--balanse")
-                {
-                    urlResponse = "https://sms.ru/my/balance?api_id=" + API + "&json=1";
+				string nmbr = args[1];
+				if (args[1] == "-balance")              //Если второй параметр -balance
+				{
+					urlResponse = "https://sms.ru/my/balance?api_id=" + API + "&json=1";
                 }
-                else
-                {
-                    urlResponse = "https://sms.ru/sms/send?api_id=" + API + "&to=" + args[1] + "&msg=" + args[2] + "&json=1";
+				else									//иначе собираем строку запроса
+				{
+					string msg = args[2];
+					urlResponse = "https://sms.ru/sms/send?api_id=" + API + "&to=" + nmbr + "&msg=" + msg + "&json=1";
                 }
-                //Console.WriteLine("Строка HTTP-запроса:");
-                //Console.WriteLine(urlResponse);
                 //отправка HTTP-запроса
                 WebRequest request = WebRequest.Create(urlResponse);
                 using (WebResponse response = request.GetResponse())
@@ -75,8 +75,6 @@ namespace Api2sms
                 Console.WriteLine("Неизвестная ошибка:");
                 Console.WriteLine(e.ToString());
             }
-            //Console.WriteLine("Нажмите Enter...");
-            //Console.Read();
         }
     }
 }
